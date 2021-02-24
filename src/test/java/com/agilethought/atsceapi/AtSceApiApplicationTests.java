7e188527cf026aeed6f6fc2b8ef560748ed86c9a
Dayana Llerenas <dllerena@ucol.mx>
2021-02-23 18:49:36 -0600

    Changes to ATSceApiApplication
    AtSceApiApplication to ATSceApiApplication

--- src/test/java/com/agilethought/atsceapi/AtSceApiApplicationTests.java
+++ src/test/java/com/agilethought/atsceapi/AtSceApiApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class AtSceApiApplicationTests {
+class ATSceApiApplicationTests {
 
 	@Test
 	void contextLoads() {
