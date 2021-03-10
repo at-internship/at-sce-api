201491fc4b3b74683b8b61954fffb8496746084e
graciela24 <mariagraciela_torres@ucol.mx>
2021-03-10 13:22:07 -0600

    Solve conflicts with develop.

--- src/main/java/com/agilethought/atsceapi/controller/UserController.java
+++ src/main/java/com/agilethought/atsceapi/controller/UserController.java
@@ -4,13 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseStatus;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import com.agilethought.atsceapi.dto.LoginData;
 import com.agilethought.atsceapi.dto.NewUserRequest;
