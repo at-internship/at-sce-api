d6c423cc608041fcb84ace647f2af86c1a19d4ea
Blanca Galindo <blanca.galindo@agilethought.com>
2021-03-11 12:53:56 -0600

    Implement Swagger

--- src/main/java/com/agilethought/atsceapi/dto/NewUserResponse.java
+++ src/main/java/com/agilethought/atsceapi/dto/NewUserResponse.java
@@ -1,9 +1,13 @@
 package com.agilethought.atsceapi.dto;
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
+@ApiModel(description = "User Response")
 public class NewUserResponse {
+	@ApiModelProperty(example = "6fbd5", value = "Unique Identifier")
 	private String id;
 }
