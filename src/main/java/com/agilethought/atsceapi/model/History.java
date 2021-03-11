f6a0000bf0d08bac3ee256545e0cca08cbf29c4b
injectionregex <jose.ochoa@agilethought.com>
2021-03-11 15:26:10 -0600

    Added service and controller layers for creation of new History through POST operation.

--- src/main/java/com/agilethought/atsceapi/model/History.java
+++ src/main/java/com/agilethought/atsceapi/model/History.java
@@ -1,17 +1,19 @@
 package com.agilethought.atsceapi.model;
 
 import com.agilethought.atsceapi.domain.FixedExpenses;
+import lombok.AllArgsConstructor;
 import lombok.Data;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 @Data
+@AllArgsConstructor
 @Document(collection = "histories")
 public class History {
     @Id
     private String id;
     private byte type;
-    private User user_id;
+    private String user_id;
     private FixedExpenses fixedExpenses;
     private int totalHours;
     private int totalDays;
