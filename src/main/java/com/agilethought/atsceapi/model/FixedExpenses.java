50e039a17ae3f061dfd77b77791b5e7787cf93be
Ale <Ale@DESKTOP-VH89K98>
2021-03-10 19:48:01 -0600

    Added history models and class mapping to DTO objects.

---
+++ src/main/java/com/agilethought/atsceapi/model/FixedExpenses.java
@@ -0,0 +1,14 @@
+package com.agilethought.atsceapi.model;
+
+import lombok.Data;
+
+@Data
+public class FixedExpenses {
+
+	private double rent;
+	private double transport;
+	private double internet;
+	private double feed;
+	private double others;
+	private double total;
+}
