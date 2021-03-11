5236c89d4d234ae4e4b28e1a018931a9d01dd806
RamOwen99 <owen.ramirez@agilethought.com>
2021-03-11 11:16:05 -0600

    Update Validator interface
    Delete blank spaces on the file, and delete an unnecessary import and the
    public word as well

--- src/main/java/com/agilethought/atsceapi/validator/Validator.java
+++ src/main/java/com/agilethought/atsceapi/validator/Validator.java
@@ -1,7 +1,5 @@
 package com.agilethought.atsceapi.validator;
 
-import com.agilethought.atsceapi.model.User;
-
 public interface Validator<E> {
-	public void validate(E object);
+	void validate(E object);
 }
