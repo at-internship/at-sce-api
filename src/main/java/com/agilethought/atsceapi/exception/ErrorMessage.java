14dd3db4baa7163292ed1c4d73f635e99e506862
RamOwen99 <owen.ramirez@agilethought.com>
2021-03-18 14:43:57 -0600

    Correct english grammar

--- src/main/java/com/agilethought/atsceapi/exception/ErrorMessage.java
+++ src/main/java/com/agilethought/atsceapi/exception/ErrorMessage.java
@@ -4,11 +4,11 @@ public class ErrorMessage {
 
     public static final String BAD_REQUEST_MESSAGE = "The field ";
     public static final String BAD_REQUEST_MESSAGE_TYPE = "needs to be 1 or 2";
-    public static final String BAD_REQUEST_MESSAGE_FIRST_NAME_LASTNAME = "is required and you don't type it";
+    public static final String BAD_REQUEST_MESSAGE_FIRST_NAME_LASTNAME = "is required and you didn't type it";
     public static final String BAD_REQUEST_MESSAGE_EMAIL = "is not valid, because it doesn't have the correct format, " +
             "try without capital letters and the format like this xxx@domail.xxx";
     public static final String BAD_REQUEST_MESSAGE_PASSWORD = "is not valid, because it doesn't have the correct format, " +
             "try with at least 10 characters, a numeric value and without capital letters characters";
-    public static final String BAD_REQUEST_MESSAGE_EMAIL_IN_DB = "can not be store, because it already exist in the Database";
+    public static final String BAD_REQUEST_MESSAGE_EMAIL_IN_DB = "can not be store, because it already exists in the Database, try with another email";
     public static final String BAD_REQUEST_MESSAGE_STATUS = "The status needs to be 0 or 1";
 }
