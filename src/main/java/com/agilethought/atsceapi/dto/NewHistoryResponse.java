aab15ec812528176c98431dcc4e59a17059ccd42
Sergio-Dom <sergio.dominguez@agilethought.com>
2021-03-29 10:58:21 -0600

    Documenting dto classes with swagger

--- src/main/java/com/agilethought/atsceapi/dto/NewHistoryResponse.java
+++ src/main/java/com/agilethought/atsceapi/dto/NewHistoryResponse.java
@@ -1,5 +1,7 @@
 package com.agilethought.atsceapi.dto;
 
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
@@ -7,6 +9,8 @@ import lombok.Setter;
 @Getter
 @Setter
 @AllArgsConstructor
+@ApiModel(description="Template that saves history info for a response")
 public class NewHistoryResponse {
+	@ApiModelProperty(value = "Unique identifier", example = "604bd54e9fb3ee3c9827408f")
     private String id;
 }
