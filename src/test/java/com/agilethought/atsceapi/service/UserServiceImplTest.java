ebea6848701d2e3625117e329c46631f4b020876
RamOwen99 <owen.ramirez@agilethought.com>
2021-04-12 10:01:31 -0500

    Update userServiceImplTest
    There was a problem in this unit test, because it throw us a
    NullPointerException, because our userServiceImpl mapper was null, and
    also, in the setup method require the before tag to start the test,
    Finally, Modify the name of the test method.

--- src/test/java/com/agilethought/atsceapi/service/UserServiceImplTest.java
+++ src/test/java/com/agilethought/atsceapi/service/UserServiceImplTest.java
@@ -1,12 +1,16 @@
 package com.agilethought.atsceapi.service;
 
 import static org.junit.Assert.assertNotNull;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyList;
 import static org.mockito.Mockito.when;
 
 import java.util.ArrayList;
 import java.util.List;
 
 import com.agilethought.atsceapi.service.implementation.UserServiceImpl;
+import ma.glasnost.orika.MapperFacade;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
@@ -23,16 +27,21 @@ public class UserServiceImplTest {
     @Mock
     private UserRepository userRepository;
 
+    @Mock
+    private MapperFacade userRepositoryMapper;
+
     @InjectMocks
     private UserServiceImpl userService;
 
+    @Before
     public void setup() {
         MockitoAnnotations.initMocks(this);
     }
 
     @Test
-    public void getAllUsersTest(){
+    public void itShouldGetAllUsers(){
         when(userRepository.findAll()).thenReturn(new ArrayList<>());
+        when(userRepositoryMapper.mapAsList(anyList(),any())).thenReturn(new ArrayList<>());
         List<UserDTO> result = userService.getAllUsers();
         assertNotNull(result);
     }
