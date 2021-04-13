ac407587e6097269010145c9e1b971e725e672f6
RamOwen99 <owen.ramirez@agilethought.com>
2021-04-12 22:49:13 -0500

    Import necessary method

--- src/test/java/com/agilethought/atsceapi/service/UserServiceImplTest.java
+++ src/test/java/com/agilethought/atsceapi/service/UserServiceImplTest.java
@@ -1,6 +1,6 @@
 package com.agilethought.atsceapi.service;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertNotNull;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyList;
 import static org.mockito.Mockito.when;
