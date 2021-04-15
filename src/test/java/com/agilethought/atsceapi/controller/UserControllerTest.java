ce7da9ae82d4a4c1ca81a415f28fcc863390431d
injectionregex <jose.ochoa@agilethought.com>
2021-04-15 09:08:21 -0500

    JUnits for User Service layer.

---
+++ src/test/java/com/agilethought/atsceapi/controller/UserControllerTest.java
@@ -0,0 +1,8 @@
+package com.agilethought.atsceapi.controller;
+
+import org.junit.runner.RunWith;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+public class UserControllerTest {
+}
